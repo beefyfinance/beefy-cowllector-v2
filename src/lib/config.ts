5ef2face06a762d62b523d1a87b6ce925e9a96ca
Clément Prévost <998369+prevostc@users.noreply.github.com>
2024-11-17 10:23:08 +0100

    Unwrap avax sooner

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -309,7 +309,7 @@ export const RPC_CONFIG: Record<Chain, RpcConfig> = {
         unwrap: {
             ...defaultUnwrapConfig,
             minAmountOfWNativeWei: bigintMultiplyFloat(ONE_ETHER, 0.05),
-            maxAmountOfNativeWei: bigintMultiplyFloat(ONE_ETHER, 0.2),
+            maxAmountOfNativeWei: bigintMultiplyFloat(ONE_ETHER, 0.5),
         },
     },
     base: {
