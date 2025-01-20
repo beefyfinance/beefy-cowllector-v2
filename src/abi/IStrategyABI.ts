325ea0e3846ea46ad79e99e327c0ab749ba721c6
prevostc <998369+prevostc@users.noreply.github.com>
2025-01-20 09:30:55 +0100

    Add strategy abi errors

--- src/abi/IStrategyABI.ts
+++ src/abi/IStrategyABI.ts
@@ -13,4 +13,35 @@ export const IStrategyABI = [
         stateMutability: 'nonpayable',
         type: 'function',
     },
+
+    // error abi to decode in case of error
+    {
+        inputs: [{ internalType: 'address', name: 'target', type: 'address' }],
+        name: 'AddressEmptyCode',
+        type: 'error',
+    },
+    {
+        inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
+        name: 'AddressInsufficientBalance',
+        type: 'error',
+    },
+    { inputs: [], name: 'FailedInnerCall', type: 'error' },
+    { inputs: [], name: 'InvalidEntry', type: 'error' },
+    { inputs: [], name: 'InvalidInput', type: 'error' },
+    { inputs: [], name: 'InvalidOutput', type: 'error' },
+    { inputs: [], name: 'InvalidTicks', type: 'error' },
+    { inputs: [], name: 'NotAuthorized', type: 'error' },
+    { inputs: [], name: 'NotCalm', type: 'error' },
+    { inputs: [], name: 'NotManager', type: 'error' },
+    { inputs: [], name: 'NotPool', type: 'error' },
+    { inputs: [], name: 'NotStrategist', type: 'error' },
+    { inputs: [], name: 'NotVault', type: 'error' },
+    { inputs: [], name: 'OverLimit', type: 'error' },
+    {
+        inputs: [{ internalType: 'address', name: 'token', type: 'address' }],
+        name: 'SafeERC20FailedOperation',
+        type: 'error',
+    },
+    { inputs: [], name: 'StrategyPaused', type: 'error' },
+    { inputs: [], name: 'TooMuchSlippage', type: 'error' },
 ] as const;
