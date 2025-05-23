fc27883d41aef10ea296ddc305fd807c7401ec0a
ReflectiveChimp <55021052+ReflectiveChimp@users.noreply.github.com>
2025-05-23 12:36:46 +0100

    remove unused exports

--- src/lib/addressbook.ts
+++ src/lib/addressbook.ts
@@ -6,11 +6,6 @@ export function getChainNativeSymbol(chain: Chain): string {
     return addressbook.addressBook[chain].native.symbol;
 }
 
-export function getChainWNativeTokenDecimals(chain: Chain): number {
-    const tokens = addressbook.addressBook[chain].tokens;
-    return tokens.WNATIVE.decimals;
-}
-
 export function getChainWNativeTokenSymbol(chain: Chain): string {
     const tokens = addressbook.addressBook[chain].tokens;
     return tokens.WNATIVE.symbol;
@@ -24,11 +19,6 @@ export function getChainWNativeTokenAddress(chain: Chain): Hex {
 export function getChainFeesTokenDecimals(chain: Chain): number {
     const tokens = addressbook.addressBook[chain].tokens;
     return tokens.FEES.decimals;
-}
-
-export function getChainFeesTokenSymbol(chain: Chain): string {
-    const tokens = addressbook.addressBook[chain].tokens;
-    return tokens.FEES.symbol;
 }
 
 export function getChainFeesTokenAddress(chain: Chain): Hex {
